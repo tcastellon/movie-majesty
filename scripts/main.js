f5ad0652f7121ae17577617c6adb891b68cfebed
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-07-16 15:09:12 -0500

    Implemented main algorithm in the main module

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+import { generateMoviesHTML } from './movie-converter.js'
+import { renderMoviesToDOM } from './movie-render.js'
+
+const movieHTML = generateMoviesHTML()
+
+renderMoviesToDOM(movieHTML)
